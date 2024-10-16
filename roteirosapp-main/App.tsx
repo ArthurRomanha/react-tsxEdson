49873b2e8c30281d2cbe469eddfd61f3b3b5225b
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-10-16 12:39:22 -0300

    Update App.tsx

---
+++ roteirosapp-main/App.tsx
@@ -0,0 +1,3 @@
+import {useState} from 'react';
+import { StyleSheet, Text, View, StatusBar, TextInput , Pletform, Pressable, ScrollView, ActivityIndicator, KeyBoard, Alert } from 'react-native';
+import {MaterialIcons} from 'expo/vector/icons';
